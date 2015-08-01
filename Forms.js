57134a308672d9752b62ca10f7c23e833e261c7a
Rianna Cantarelli <rianna.aeon@gmail.com>
2015-07-31 22:15:14 -0300

    Mod Test

--- Forms.js
+++ Forms.js
@@ -17,7 +17,7 @@
 
 
 
-// --------------------
+// ---------------------
 // Caso não exista, inicia objeto CodeCraft
 var CodeCraft = (CodeCraft || function () { });
 if(typeof(CodeCraft) === 'function') { CodeCraft = new CodeCraft(); };
